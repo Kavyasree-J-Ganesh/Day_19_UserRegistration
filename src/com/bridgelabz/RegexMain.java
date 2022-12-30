1a58474dea9cb3c309548acb1432508ce790f7bd
Kavyasree-J-Ganesh <kavyasreejganesh97@gmail.com>
2022-12-31 02:34:46 +0530

    [Kavyasree J ganesh] Regex User Registration problem

--- src/com/bridgelabz/RegexMain.java
+++ src/com/bridgelabz/RegexMain.java
@@ -1,10 +1,24 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class RegexMain {
     public static void main(String[] args) {
-        Regex.validateFirstName("Kavya");
-        Regex.validateFirstName("kavya");
-        Regex.validateLastName("Ganesh");
-        Regex.validateLastName("ganesh");
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter a Firstname");
+        String firstName = scan.nextLine();
+        Regex.validateFirstName(firstName);
+        System.out.println("Enter a Firstname");
+        firstName = scan.nextLine();
+        Regex.validateFirstName(firstName);
+
+        System.out.println("Enter a Lastname");
+        String lastName = scan.nextLine();
+        Regex.validateLastName(lastName);
+        System.out.println("Enter a Lastname");
+        lastName = scan.nextLine();
+        Regex.validateLastName(lastName);
+
     }
 }
